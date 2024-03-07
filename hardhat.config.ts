1aebf8539502596110b2ea29f3a1189e38a8e536
Mintah <130777897+Hyper-Tally@users.noreply.github.com>
2024-03-07 16:47:17 +0300

    Add files via upload

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,58 +1,50 @@
-require("./tasks/diamondABI.js");
-
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@openzeppelin/hardhat-upgrades";
-import "hardhat-gas-reporter";
 import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "@openzeppelin/hardhat-upgrades";
+
 dotenv.config();
 
-//@notice just for testing
-/*
-module.exports = {
-  solidity: {
-    version: "0.8.17",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 1,
-      },
-    },
-  },
-};
-*/
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: {
-    version: "0.8.17",
-    settings: {
-      viaIR: true,
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
+  solidity: "0.8.4",
+  defaultNetwork: 'development',
   networks: {
-    // hardhat: {
-    //   forking: {
-    //     url: process.env.MUMBAI_URL || "",
-    //   },
-    //   // blockGasLimit: 20000000,
-    //   // gas: 12000000,
-    //   // allowUnlimitedContractSize: true,
-    // },
-    local: {
-      url: process.env.URL || "",
+    bsctestnet: {
+      url: process.env.BSCTESTNET_URL || "",
       accounts:
-        process.env.PRIVATE_KEY !== undefined
-          ? [process.env.PRIVATE_KEY]
-          : [],
-      blockGasLimit: 20000000,
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    development: {
+      url: process.env.DEV_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
   gasReporter: {
-    enabled: false,
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
+
 export default config;
